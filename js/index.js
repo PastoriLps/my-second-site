7cf186fd98775cd9d4d4c18eeb6ea4b2ba4bbbfe
PastoriLps <rafael211295@gmail.com>
2021-03-30 10:54:44 -0300

    update

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector(".nav");
-const btnMenu = document.querySelector(".nav__menu");
-
-btnMenu.addEventListener('click', function(){
-    menu.classList.add("menu__open");
-});
